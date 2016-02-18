1a246fb703d2c14d2685ac01db412ff4dd2f46ee
dakatapetrov <daka7a@gmail.com>
2016-02-18 08:03:05 +0200

    Use kNN instead of SVR

--- creeper_pp/personality_predictor.py
+++ creeper_pp/personality_predictor.py
@@ -1,13 +1,18 @@
 from sklearn import svm
 import numpy as np
+import re
+from sklearn.preprocessing import normalize
+from sklearn.neighbors import KNeighborsRegressor
 
 class PersonalityPredictor(object):
+    nn = 10
+
     def __init__(self):
-        self.o_clf = svm.SVR()
-        self.c_clf = svm.SVR()
-        self.e_clf = svm.SVR()
-        self.a_clf = svm.SVR()
-        self.n_clf = svm.SVR()
+        self.o_clf = KNeighborsRegressor(n_neighbors=self.nn)
+        self.c_clf = KNeighborsRegressor(n_neighbors=self.nn)
+        self.e_clf = KNeighborsRegressor(n_neighbors=self.nn)
+        self.a_clf = KNeighborsRegressor(n_neighbors=self.nn)
+        self.n_clf = KNeighborsRegressor(n_neighbors=self.nn)
         self.features = []
         self.o_value = []
         self.c_value = []
@@ -17,31 +22,34 @@ class PersonalityPredictor(object):
 
     def register(self, data):
         for user_id in data:
-            self.o_value.push(data[user_id]['o'])
-            self.c_value.push(data[user_id]['c'])
-            self.e_value.push(data[user_id]['e'])
-            self.a_value.push(data[user_id]['a'])
-            self.n_value.push(data[user_id]['n'])
-            self.features.push(data[user_id]['f'])
+            if 'f' in data[user_id]:
+                self.o_value.append(self.make_float(data[user_id]['o']))
+                self.c_value.append(self.make_float(data[user_id]['c']))
+                self.e_value.append(self.make_float(data[user_id]['e']))
+                self.a_value.append(self.make_float(data[user_id]['a']))
+                self.n_value.append(self.make_float(data[user_id]['n']))
+                self.features.append(data[user_id]['f'])
+            else:
+                break
+
+    def make_float(self, value):
+        if isinstance(value, basestring):
+            return float(re.sub("[^0-9.]", "", value))
+        else:
+            return float(value)
 
     def train(self):
-        features = np.array(self.features)
-        o_value = np.array(self.o_value)
-        c_value = np.array(self.c_value)
-        e_value = np.array(self.e_value)
-        a_value = np.array(self.a_value)
-        n_value = np.array(self.n_value)
-        self.o_clf.fit(features, o_value)
-        self.c_clf.fit(features, c_value)
-        self.e_clf.fit(features, e_value)
-        self.a_clf.fit(features, a_value)
-        self.n_clf.fit(features, n_value)
+        self.features = normalize(self.features)
+        print self.o_clf.fit(self.features, self.o_value)
+        print self.c_clf.fit(self.features, self.c_value)
+        print self.e_clf.fit(self.features, self.e_value)
+        print self.a_clf.fit(self.features, self.a_value)
+        print self.n_clf.fit(self.features, self.n_value)
 
     def predict(self, features):
-        features_np = np.array(features)
-        o = self.o_clf.predict(features_np)
-        c = self.c_clf.predict(features_np)
-        e = self.e_clf.predict(features_np)
-        a = self.a_clf.predict(features_np)
-        n = self.n_clf.predict(features_np)
+        o = self.o_clf.predict([features])
+        c = self.c_clf.predict([features])
+        e = self.e_clf.predict([features])
+        a = self.a_clf.predict([features])
+        n = self.n_clf.predict([features])
         return [o, c, e, a, n]
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from creeper_pp.shell_service import ShellService
 from creeper_pp.mrc_service import MrcService
 from creeper_pp.personality_predictor import PersonalityPredictor
 
+from tweepy.error import TweepError
+
 auth = tweepy.OAuthHandler(twitter_config.consumer_key, twitter_config.consumer_secret)
 auth.set_access_token(twitter_config.access_token, twitter_config.access_token_secret)
 
@@ -18,13 +20,45 @@ api = tweepy.API(auth)
 ie = InformationExtractor(api)
 
 bigfive_data = os.getcwd() + '/resources/bigfive_data.json'
+print 'reading json...'
 with open(bigfive_data) as data_file:
     data = json.load(data_file)
 
-for username in data:
-    user = ie.extract(username, 200)
-    fe = FeaturesExtractor(user)
-    data[username]['f'] = fe.get_features()
+to_remove = []
 
-    print(username)
-    print(data[username])
+cnt = 100
+i = 0
+
+for username in data:
+    print 'extracting information for user ' + username + '...'
+    try:
+        user = ie.extract(username, 200)
+        fe = FeaturesExtractor(user)
+        data[username]['f'] = fe.get_features()
+        i += 1
+        if i == cnt:
+            break
+    except ZeroDivisionError:
+        print 'meh ' + username
+        if username in data:
+            to_remove.append(username)
+    except TweepError:
+        print 'capacity'
+        if username in data:
+            to_remove.append(username)
+
+for uname in to_remove:
+    if uname in data:
+        del data[uname]
+
+pp = PersonalityPredictor()
+print 'registering data...'
+pp.register(data)
+print 'training...'
+pp.train()
+
+print 'predicting'
+user_ = ie.extract('skanev', 200)
+user_fe = FeaturesExtractor(user_)
+user_features = user_fe.get_features()
+print pp.predict(user_features)
